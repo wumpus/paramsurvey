ebe28f0472fa66c8ce54e0f6c6a5d4ef6abcef78
Greg Lindahl <lindahl@pbm.com>
2020-07-18 21:36:24 -0700

    try to get CI working

--- test/test-ray.sh
+++ test/test-ray.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ ! -z "$ONLY_BUILTINS" ]; then
+    exit 0
+fi
+
 PORT=6379
 REDIS_PASSWORD=thehfhghedhdjfhgfhdhdhdf
 echo $(hostname):$PORT $REDIS_PASSWORD > ~/.ray-test-72363726-details
