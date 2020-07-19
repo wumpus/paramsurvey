4c1816ad4b3025155d4d60e4c83ff08e0666bda7
Greg Lindahl <lindahl@pbm.com>
2020-07-18 19:12:01 -0700

    strays

---
+++ paramsurvey/utils.py
@@ -0,0 +1,60 @@
+import time
+import sys
+from collections import defaultdict
+
+from . import stats
+
+
+def accumulate_return(user_ret, system_kwargs, user_kwargs):
+    if 'user_ret' not in system_kwargs:
+        system_kwargs['user_ret'] = []
+    system_kwargs['user_ret'].append(user_ret)
+
+
+def report_progress(system_kwargs):
+    t = time.time()
+    if t - system_kwargs['progress_last'] > system_kwargs['progress_dt']:
+        system_kwargs['progress_last'] = t
+        print(system_kwargs['name'], 'progress:',
+              ', '.join([k+': '+str(v) for k, v in system_kwargs['progress'].items()]),
+              file=sys.stderr)
+        sys.stderr.flush()
+
+
+def remaining(system_kwargs):
+    progress = system_kwargs['progress']
+    return progress['started'] - progress.get('retired', 0)
+
+
+def get_work_units(work, group_size):
+    work_units = []
+    for _ in range(group_size):
+        try:
+            work_units.append(work.pop(0))
+        except IndexError:
+            pass
+    return work_units
+
+
+def map_prep(name, chdir, outfile, out_subdirs, work_len):
+    print('starting work on', name, file=sys.stderr)
+    sys.stderr.flush()
+
+    system_kwargs = {}
+    if chdir:
+        system_kwargs['chdir'] = chdir
+    if outfile:
+        system_kwargs['outfile'] = outfile
+    if out_subdirs:
+        system_kwargs['out_subdirs'] = out_subdirs
+    if name:
+        system_kwargs['name'] = name
+
+    system_stats = stats.StatsObject()
+    progress = defaultdict(int)
+    progress['total'] = work_len
+    system_kwargs['progress'] = progress
+    system_kwargs['progress_last'] = 0.
+    system_kwargs['progress_dt'] = 0.
+
+    return system_stats, system_kwargs
