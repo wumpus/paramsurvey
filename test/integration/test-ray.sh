95ed61778ff17f928738b1ee05f2b7135dc97a2a
Greg Lindahl <lindahl@pbm.com>
2020-08-15 18:44:16 -0700

    test ONLY_BUILTINS

--- test/integration/test-ray.sh
+++ test/integration/test-ray.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+if [ ! -z "$COVERAGE" ]; then
+    COVERAGE="--cov-report= --cov-append --cov-branch --cov paramsurvey -v -v"
+fi
+
 if [ ! -z "$ONLY_BUILTINS" ]; then
-    exit 0
+    PARAMSURVEY_BACKEND=ray pytest $COVERAGE $1
+    # expected to fail
+    # not exercised in "make test", but it is in travis CI
+    test $0 -ne 0 || exit 1
 fi
 
 PORT=6379
@@ -11,10 +18,6 @@ echo $(hostname):$PORT $REDIS_PASSWORD > ~/.ray-test-72363726-details
 GIGABYTE=1000000000  # close enough
 
 ray start --head --redis-port=$PORT --redis-password=$REDIS_PASSWORD --memory $GIGABYTE --object-store-memory $GIGABYTE --redis-max-memory $GIGABYTE
-
-if [ ! -z "$COVERAGE" ]; then
-    COVERAGE="--cov-report= --cov-append --cov-branch --cov paramsurvey -v -v"
-fi
 
 # in order to find both the uninstalled paramsurvey and our test program,
 # which ray needs to pickle, the PYTHONPATH must be manipulated. See
