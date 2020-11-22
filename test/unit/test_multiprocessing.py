bbab79419d4117dc03fcdd7126559893b4965411
Greg Lindahl <lindahl@pbm.com>
2020-11-22 08:17:34 -0800

    fix this new test to actually work

--- test/unit/test_multiprocessing.py
+++ test/unit/test_multiprocessing.py
@@ -1,23 +1,26 @@
 import platform
 import os
+import pytest
+
 import paramsurvey.psmultiprocessing
 
 
 def test__core_count():
     psys = platform.system()
     if psys == 'Darwin':
-        assert os.cpu_count == paramsurvey.psmultiprocessing._core_count()
+        assert os.cpu_count() == paramsurvey.psmultiprocessing._core_count()
     elif psys == 'Windows':
         # len(psutil.Process().cpu_affinity()) -- not yet implemented by us
-        assert os.cpu_count == paramsurvey.psmultiprocessing._core_count()
+        assert os.cpu_count() == paramsurvey.psmultiprocessing._core_count()
     elif psys == 'Linux':
         if 'sched_getaffinity' not in dir(os):
-            assert os.cpu_count == paramsurvey.psmultiprocessing._core_count()
+            assert os.cpu_count() == paramsurvey.psmultiprocessing._core_count()
             return
+
         start = os.sched_getaffinity(0)
         assert paramsurvey.psmultiprocessing._core_count() == len(start)
         if len(start) == 1:
-            # so sad
+            pytest.skip('cannot test Linux setaffinity core_count because we only have 1 core')
             return
 
         first = start.copy().pop()
