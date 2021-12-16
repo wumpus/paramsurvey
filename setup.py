a92966833bda074b6aea97d9217af71124de7bb9
Greg Lindahl <lindahl@pbm.com>
2021-12-16 14:20:25 -0800

    comment

--- setup.py
+++ setup.py
@@ -73,6 +73,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        #'Programming Language :: Python :: 3.10',  # awaiting a ray wheel, https://github.com/ray-project/ray/issues/19116
         'Programming Language :: Python :: 3 :: Only',
     ],
 )
